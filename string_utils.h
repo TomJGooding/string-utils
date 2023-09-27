660ed9f124fe460950b72cc811e625f1de2bba58
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-09-27 09:43:36 +0100

    feat: add token array (#2)
    * feat: add token array
    
    * feat: add token array

--- string_utils.h
+++ string_utils.h
@@ -1,1 +1,11 @@
 char *string_from_file(char const *filename);
+
+typedef struct TokenArray {
+    char **elements;
+    char *base_string;
+    int length;
+} TokenArray;
+
+TokenArray *strtok_to_array(char *str, char const *delim);
+
+void free_token_array(TokenArray *array);

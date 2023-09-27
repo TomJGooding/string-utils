9da627a12422dbe90a9c39aa0c0e63c00b25315a
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-09-27 09:36:16 +0100

    feat: add token array

--- string_utils.c
+++ string_utils.c
@@ -1,3 +1,5 @@
+#include "string_utils.h"
+
 #include <glib.h>
 #include <stdio.h>
 
@@ -25,4 +27,30 @@ char *string_from_file(char const *filename) {
         return NULL;
     }
     return string;
+}
+
+TokenArray *strtok_to_array(char *str, char const *delim) {
+    TokenArray *array = malloc(sizeof(TokenArray));
+    *array = (TokenArray){.base_string = str};
+    char *scratch = NULL;
+    char *token = strtok_r(str, delim, &scratch);
+    if (!token) {
+        return NULL;
+    }
+    while (token) {
+        array->elements =
+            realloc(array->elements, sizeof(char *) * ++(array->length));
+        array->elements[array->length - 1] = token;
+        token = strtok_r(NULL, delim, &scratch);
+    }
+    return array;
+}
+
+void free_token_array(TokenArray *array) {
+    if (array == NULL) {
+        return;
+    }
+    free(array->base_string);
+    free(array->elements);
+    free(array);
 }
